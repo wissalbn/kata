170603057428a5feb05e4983fe1e907d8cb9a1b0
Nadabenali <nadabenali619@gmail.com>
2025-12-10 01:32:21 +0100

    ajout du mot cle final qui empêche la réaffectation dans la mehtode abilitiesPerTypeAndLevel

--- src/main/java/re/forestier/edu/rpg/UpdatePlayer.java
+++ src/main/java/re/forestier/edu/rpg/UpdatePlayer.java
@@ -8,7 +8,7 @@ public class UpdatePlayer {
     private final static String[] objectList = {"Lookout Ring : Prevents surprise attacks","Scroll of Stupidity : INT-2 when applied to an enemy", "Draupnir : Increases XP gained by 100%", "Magic Charm : Magic +10 for 5 rounds", "Rune Staff of Curse : May burn your ennemies... Or yourself. Who knows?", "Combat Edge : Well, that's an edge", "Holy Elixir : Recover your HP"
     };
 
-    public static HashMap<String, HashMap<Integer, HashMap<String, Integer>>> abilitiesPerTypeAndLevel() {
+    public static final HashMap<String, HashMap<Integer, HashMap<String, Integer>>> abilitiesPerTypeAndLevel() {
         HashMap<String, HashMap<Integer, HashMap<String, Integer>>> abilitiesPerTypeAndLevel = new HashMap<>();
 
         HashMap<Integer, HashMap<String, Integer>> adventurerMap = new HashMap<>();
